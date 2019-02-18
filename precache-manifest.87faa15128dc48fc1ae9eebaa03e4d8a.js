6b1dc31cb2cd3d3b88332f69d39b73d8edcdde22
Christian Huffman <cjh605@gmail.com>
2019-02-18 16:57:35 -0500

    Updates

---
+++ precache-manifest.87faa15128dc48fc1ae9eebaa03e4d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a22faca2f410a15968b",
+    "url": "/ReactInfiniteScrollChallenge/static/js/runtime~main.1a22faca.js"
+  },
+  {
+    "revision": "6a1cef57aca4e2406a35",
+    "url": "/ReactInfiniteScrollChallenge/static/js/main.6a1cef57.chunk.js"
+  },
+  {
+    "revision": "0cb4cd620d05d5faf7b6",
+    "url": "/ReactInfiniteScrollChallenge/static/js/2.0cb4cd62.chunk.js"
+  },
+  {
+    "revision": "6a1cef57aca4e2406a35",
+    "url": "/ReactInfiniteScrollChallenge/static/css/main.da1d7482.chunk.css"
+  },
+  {
+    "revision": "a21d71afccdd803eba16d84dced4c2b9",
+    "url": "/ReactInfiniteScrollChallenge/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ReactInfiniteScrollChallenge/precache-manifest.87faa15128dc48fc1ae9eebaa03e4d8a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ReactInfiniteScrollChallenge/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
