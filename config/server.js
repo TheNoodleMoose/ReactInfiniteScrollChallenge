fa44d8651fea30e140360668dcb3edcf8e5eb818
Christian Huffman <cjh605@gmail.com>
2019-02-18 16:51:19 -0500

    Adding initial files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = {
+  APPLICATION_ID:
+    process.env.APPLICATION_ID ||
+    "aec258207daf37a856571165e8da8ce25e15c5254e82488ea45c888ad333ab5d",
+  SECRET:
+    process.env.SECRET ||
+    "b7f2f6a7b938a52151977dac5b2803388fe8ce78c39f941db489ede15ef9bd29",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
